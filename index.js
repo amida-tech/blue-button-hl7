0fd586ccd46ebaf0fa7495e7e168dbaab794c7dd
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-12-12 02:12:05 -0500

    travis debug

--- index.js
+++ index.js
@@ -173,10 +173,12 @@ function translate(data) {
 
     var msg = hl7.translate(hl7.parseString(data));
 
+    console.log(msg);
+
     //process message by segment
     for (var seg in msg) {
         var segment = msg[seg];
-        //console.log(segment);
+        console.log(segment);
 
         if (segment && segment["Segment"] === "PID") {
             bb["demographics"] = demographics(segment);
